763c39a6cb45b92d771b72d9fc567a3108cf5bdd
Moavia Amir <contactmuawia@gmail.com>
2025-05-04 23:32:37 +0500

    Create solution.py

---
+++ Eassy/1128. Number of Equivalent Domino Pairs/solution.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def numEquivDominoPairs(self, dominoes):
+        """
+        :type dominoes: List[List[int]]
+        :rtype: int
+        """
+        count_map = {}
+        result = 0
+
+        for a, b in dominoes:
+            key = tuple(sorted((a,b)))
+
+            if key in count_map:
+                result += count_map[key]          
+                count_map[key] += 1
+            else:
+                count_map[key] = 1
+
+        return  result             
