02126cad664d80c0946f24cf84e45735418322f1
Moavia Amir <contactmuawia@gmail.com>
2025-05-11 19:01:27 +0500

    Create solution.py

---
+++ Eassy/1550. Three Consecutive Odds/solution.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def threeConsecutiveOdds(self, arr):
+        """
+        :type arr: List[int]
+        :rtype: bool
+        """
+        count = 0
+        for i in arr:
+            if i % 2 != 0:
+                count += 1
+                if count == 3:
+                    return True
+            else:
+                count = 0
+            
+        return False
+        
