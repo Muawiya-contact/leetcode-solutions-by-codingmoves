e32b1155cac9f64dac4b25ad0d68d6f58117861b
Moavia Amir <contactmuawia@gmail.com>
2025-07-07 22:44:52 +0500

    Create Solution.py

---
+++ Medium/1353. Maximum Number of Events That Can Be Attended/Solution.py
@@ -0,0 +1,30 @@
+import heapq
+class Solution(object):
+    def maxEvents(self, events):
+        """
+        :type events: List[List[int]]
+        :rtype: int
+        """
+        events.sort()
+        min_heap=[]
+        i = 0
+        n = len(events)
+        day = 1
+        attended = 0
+
+        while i < n or min_heap:
+            while i < n and events[i][0] == day:
+                heapq.heappush(min_heap, events[i][1])
+                i += 1
+
+            while min_heap and min_heap[0] < day:
+                heapq.heappop(min_heap)
+
+            if min_heap:
+                heapq.heappop(min_heap)
+                attended += 1
+            day += 1
+
+        return attended                
+
+        
