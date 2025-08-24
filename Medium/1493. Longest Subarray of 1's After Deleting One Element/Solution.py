de970587d0bfeebcb52af46108a7f9f2b2039175
Moavia Amir <contactmuawia@gmail.com>
2025-08-24 22:36:16 +0500

    Create Solution.py

---
+++ Medium/1493. Longest Subarray of 1's After Deleting One Element/Solution.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def longestSubarray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        left = 0
+        zero_count = 0
+        max_len = 0
+        for right in range(len(nums)):
+            if nums[right] == 0:
+                zero_count += 1
+            while zero_count > 1:
+                if nums[left] == 0:
+                    zero_count -= 1
+                left += 1
+            max_len = max(max_len, right - left + 1)
+        return max_len - 1
+        
