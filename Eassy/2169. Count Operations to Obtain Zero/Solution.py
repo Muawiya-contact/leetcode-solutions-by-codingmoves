b1f54f2cac69109f933d256976b4e66debcb6ac8
Moavia Amir <contactmuawia@gmail.com>
2025-11-09 22:38:55 +0500

    Create Solution.py

---
+++ Eassy/2169. Count Operations to Obtain Zero/Solution.py
@@ -0,0 +1,19 @@
+print("Try programiz.pro")
+
+class Solution(object):
+    def countOperations(self, num1, num2):
+        """
+        :type num1: int
+        :type num2: int
+        :rtype: int
+        """
+        count = 0
+        while num1 != 0 and num2 != 0:   
+            if num1 >= num2:
+                num1 -= num2
+            else:
+                num2 -= num1
+            count += 1
+        return count
+
+print(Solution().countOperations(2, 3)) 
