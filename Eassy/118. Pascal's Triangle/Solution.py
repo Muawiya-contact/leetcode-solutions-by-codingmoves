62c69da19e7bac6300d428ec95a2d9df31e69ae4
Moavia Amir <contactmuawia@gmail.com>
2025-11-15 23:56:19 +0500

    Update Solution.py

--- Eassy/118. Pascal's Triangle/Solution.py
+++ Eassy/118. Pascal's Triangle/Solution.py
@@ -3,7 +3,7 @@ class Solution:
         triangle = []
 
         for i in range(numRows):
-            row = [1] * (i + 1)  # Start with all 1s
+            row = [1] * (i + 1)  
             for j in range(1, i):
                 row[j] = triangle[i-1][j-1] + triangle[i-1][j]
             triangle.append(row)
