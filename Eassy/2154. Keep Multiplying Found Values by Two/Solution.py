7fa8a75e658ede6f458fa70fca53691653f7db26
Moavia Amir <contactmuawia@gmail.com>
2025-11-19 21:04:05 +0500

    Create Solution.py

---
+++ Eassy/2154. Keep Multiplying Found Values by Two/Solution.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def findFinalValue(self, nums, original):
+        """
+        :type nums: List[int]
+        :type original: int
+        :rtype: int
+        """
+        nums.sort()
+        for i in nums:
+            if i == original:
+                original = 2 * original
+
+        return original
