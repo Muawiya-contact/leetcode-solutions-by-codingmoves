06948f45cc282af663e56bf26154d7a854ea30d2
Moavia Amir <contactmuawia@gmail.com>
2026-01-06 23:42:57 +0500

    Update Solution.py

--- Eassy/1018. Binary Prefix Divisible By 5/Solution.py
+++ Eassy/1018. Binary Prefix Divisible By 5/Solution.py
@@ -1,5 +1,5 @@
 class Solution:
-    def prefixesDivBy5(self, nums: List[int]) -> List[bool]:
+    def prefixesDivBy5(self, nums):
         val = 0
         for i in range(len(nums)):
             val = ((val << 1) + nums[i]) % 5
