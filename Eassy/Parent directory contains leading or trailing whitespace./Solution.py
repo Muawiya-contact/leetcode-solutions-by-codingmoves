754741ecb87fc6152525e19c6de6298d9df90421
Moavia Amir <contactmuawia@gmail.com>
2026-01-25 22:20:13 +0500

    Create Solution.py

---
+++ Eassy/Parent directory contains leading or trailing whitespace./Solution.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def minimumDifference(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        if k == 0:
+            return 0
+        
+        nums.sort()
+        ans = float('inf')
+        for i in range(len(nums) -k +1):
+            ans = min(ans, nums[i + k-1] - nums[i])
+
+        return ans
