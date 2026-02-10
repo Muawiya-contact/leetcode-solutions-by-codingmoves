3f2ecd5bcae28d9c0682e2a3d4f7b05d2179c30b
Moavia Amir <contactmuawia@gmail.com>
2026-02-10 23:13:46 +0500

    Implement longestBalanced function to find max length

---
+++ Medium/3719. Longest Balanced Subarray I/Solution.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def longestBalanced(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+
+        n = len(nums)
+        max_len = 0
+
+        for i in range(n):
+            evens = set()
+            odds = set()
+            for j in range(i, n):
+                if nums[j] % 2 == 0:
+                    evens.add(nums[j])
+                else:
+                    odds.add(nums[j])
+                if len(evens) == len(odds):
+                    max_len = max(max_len, j - i + 1)
+        return max_len
+        
